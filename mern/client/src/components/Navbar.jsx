c7662df108019664f5781dd538e5768e9f17dcc5
Sushant Rana <168199532+sushantrana-dev@users.noreply.github.com>
2024-07-03 20:29:25 +0530

    Update Navbar.jsx

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ export default function Navbar() {
   const isDBRoute = location.pathname.includes('/create');
   return (
     <div>
-      <nav className="flex justify-between items-center mb-6 ">
-        <NavLink to="/"><Space align="center"> <img alt="MongoDB logo" className="h-20 inline" src={deliverySvg}></img> <span className="mt-5 text-white">CARGO MUNSHI </span></Space></NavLink>
+      <nav className="flex justify-between items-center mb-6 nav-bar-mobile">
+        <NavLink to="/"><Space align="center"> <img alt="CargoMunshi logo" className="h-20 inline rounded-3xl" src={deliverySvg}></img> <span className="mt-5 text-white">CARGO MUNSHI </span></Space></NavLink>
         {!isDBRoute && 
         <Tooltip title="DB related operations can be performed here">
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
